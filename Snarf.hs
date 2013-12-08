f1d17f29fbb73748d251dee802afa398558770c9
Thies Heidecke <halcyon10@gmail.com>
2013-12-08 19:11:30 +0100

    added getConnectedNodesBy and Node Functor implementation
    added getConnectedNodesBy to extract node connectivity information from a Network. Also made GeneralNode an instance of Functor and renamed SparseNode to Node.

--- Snarf.hs
+++ Snarf.hs
@@ -1,12 +1,19 @@
-type Activity = Double
-
-data SparseNode = SparseNode { 
-      sparseNodeActivity :: Activity          -- ^ current activation output level. Has to be updated manually (through evaluate)
+data GeneralNode a = GeneralNode { 
+    nodeActivity :: a          -- ^ current activation output level. Has to be updated manually (through evaluate)
 }
 
-instance Show SparseNode where
-   show (SparseNode a) = show . myround 100 $ a
-                       where myround s = (/s) . fromIntegral . round . (*s)
+type Node = GeneralNode Activity
+newtype Activity = Activity { getActivity :: Double }
+
+instance (Show a) => Show (GeneralNode a) where
+   show = show . nodeActivity
+
+instance Show Activity where
+  show a = show . myround 100 $ getActivity a
+         where myround s = (/s) . fromIntegral . round . (*s)
+
+instance Functor GeneralNode where
+  fmap f (GeneralNode a) = GeneralNode (f a)
 
 class HasActivity n where
     evolve :: n -> n
@@ -17,11 +24,20 @@ data Network = Network {
   networkArchitecture :: Architecture         -- ^ List of node counts for each layer
 } deriving (Show)
 
+getForwardNodes  = getConnectedNodesBy fst snd
+getBackwardNodes = getConnectedNodesBy snd fst
+
+getConnectedNodesBy :: (Edge -> NodeIndex) -> (Edge -> NodeIndex) -> Network -> NodeIndex -> [NodeIndex]
+getConnectedNodesBy f g n i = map g . (filter ((==i) . f)) . map getEdgeFromWeightedEdge . networkWeightedEdges $ n
+
 type Architecture = [Int]
 type Layer = Network
 
-type Nodes = [SparseNode]
+type Nodes = [Node]
 type WeightedEdges = [(Weight, Edge)]
+getWeightFromWeightedEdge = fst
+getEdgeFromWeightedEdge   = snd
+
 type Edge = (NodeIndex, NodeIndex)
 type NodeIndex = Int
 type Weight = Double
@@ -59,7 +75,7 @@ createLayer n = Network nodes [] [n]
               where nodes = take n . repeat $ singletonNode 0.5
 
 -- | singleton node 
-singletonNode a = SparseNode a
+singletonNode a = GeneralNode $ Activity a
 
 -- | add a one layer network with full connectivity to last layer of old network
 stackLayer :: Network -> Layer -> Network
@@ -81,4 +97,4 @@ combinePairs f xs = map (uncurry f) $ zip (init xs) (tail xs)
 
 -- Test data
 
-testnetwork = createNetwork [1, 1]
+testnetwork = createNetwork [1, 2, 1]
